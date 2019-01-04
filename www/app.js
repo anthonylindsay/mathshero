84fbf15c5d90557c5748d1015746487429a87b1f
Anthony Lindsay <anthony@netrunner.ie>
2019-01-04 08:07:06 +0000

    Coding style

--- www/app.js
+++ www/app.js
@@ -2,11 +2,11 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/www/app/sw.js')
   .then(function(reg) {
-    if(reg.installing) {
+    if (reg.installing) {
       console.log('Service worker installing');
-    } else if(reg.waiting) {
+    } else if (reg.waiting) {
       console.log('Service worker installed');
-    } else if(reg.active) {
+    } else if (reg.active) {
       console.log('Service worker active');
     }
 
